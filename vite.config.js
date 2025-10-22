0ea28ecdc4aa0232debe9a197df7936b394de7d7
alvinceu <erusalimcevp@gmail.com>
2025-10-22 13:13:15 +0300

    feat: implement lab3

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+        lab1: resolve(__dirname, "lab1/index.html"),
+        lab2: resolve(__dirname, "lab2/index.html"),
+        lab3: resolve(__dirname, "lab3/index.html"),
+        lab3about: resolve(__dirname, "lab3/about/index.html"),
+        lab3blog: resolve(__dirname, "lab3/blog/index.html"),
+        lab3catalog: resolve(__dirname, "lab3/catalog/index.html"),
+			},
+		},
+	},
+})
