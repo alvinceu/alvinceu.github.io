f2064a35a0cec8a6a5008c8c54fad6f03bbb0f23
alvinceu <erusalimcevp@gmail.com>
2025-11-02 22:21:39 +0300

    feat: lab 3 api + title screen

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite';
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
@@ -17,4 +19,7 @@ export default defineConfig({
 			},
 		},
 	},
+  plugins: [
+    tailwindcss(),
+  ],
 })
